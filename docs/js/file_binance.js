0688b4e652c5513de99f97c045dc5c8f6d16ece9
kenj0 <32511018+kenj0@users.noreply.github.com>
2018-01-21 03:04:05 +0900

    Support transaction fee calculation for Binance trade history.

--- docs/js/file_binance.js
+++ docs/js/file_binance.js
@@ -4,9 +4,10 @@ var g_binance_eth_trade_list = ["NULSETH", "ASTETH", "EOSETH", "SNTETH", "MCOETH
 var g_binance_usdt_trade_list = ["BTCUSDT", "BCCUSDT", "LTCUSDT", "NEOUSDT", "BNBUSDT", "ETHUSDT"]
 
 
-function formatBinanceDateTime(datetime) {
+function formatBinanceDateTime(datetime, delay = 0) {
   // console.log(datetime);
   dateObj = new Date(datetime + " UTC");  // convert to Local time
+  dateObj.setTime(dateObj.getTime() + delay * 1000);
   buff = dateObj.getFullYear();
   buff += "/" + ('0' + (dateObj.getMonth() + 1)).slice(-2);
   buff += "/" + ('0' + dateObj.getDate()).slice(-2);
@@ -17,68 +18,119 @@ function formatBinanceDateTime(datetime) {
   return buff;
 };
 
+function convertBinanceTransaction(each_history, first_alert) {
+  var transaction = {
+    "datetime" : formatBinanceDateTime(each_history.Date),
+    "buyCoin" : "",
+    "buyAmount" : 0,
+    "sellCoin" : "",
+    "sellAmount" : 0,
+    "isAltTrade" : true,  // allways true for binance market tradeing
+    "altJPY" : "---",
+    "marketplace" : "Binance",
+    "comment" : ("rate=" + each_history.Price)
+  };
+  var market = "";
+  if (g_binance_bnb_trade_list.indexOf(each_history.Market) != -1) {
+    market = "BNB";
+  } else if (g_binance_btc_trade_list.indexOf(each_history.Market) != -1) {
+    market = "BTC";
+  } else if (g_binance_eth_trade_list.indexOf(each_history.Market) != -1) {
+    market = "ETH";
+  } else if (g_binance_usdt_trade_list.indexOf(each_history.Market) != -1) {
+    market = "USDT";
+  } else {
+    if (first_alert) {
+      alert("Error: Could not parse Market column."); first_alert = false;
+    }
+  }
+  var key_coin = market;
+  var target_coin = each_history.Market.substring(0, each_history.Market.indexOf(market));
+  if (getCoinAlias(target_coin) != null) {
+    target_coin = getCoinAlias(target_coin);
+  }
+  var key_coin_price = getJpyPrice(key_coin, transaction["datetime"].split(" ")[0]);
+  if (key_coin_price != null) {
+    if (each_history.Type == "BUY") {
+      transaction["buyCoin"] = target_coin;
+      transaction["sellCoin"] = key_coin;
+      transaction["buyAmount"] = each_history.Amount;
+      transaction["sellAmount"] = each_history.Total;
+      transaction["altJPY"] = key_coin_price * each_history.Total;
+    } else if (each_history.Type == "SELL") {
+      transaction["buyCoin"] = key_coin;
+      transaction["sellCoin"] = target_coin;
+      transaction["buyAmount"] = each_history.Total;
+      transaction["sellAmount"] = each_history.Amount;
+      transaction["altJPY"] = key_coin_price * each_history.Total;
+    } else {
+      if (first_alert) {
+        alert("Error: Could not parse Type column."); first_alert = false;
+      }
+    }
+    transaction["comment"] += ", " + key_coin + "/JPY=" + key_coin_price;
+    // console.log(each_history);
+    // console.log(transaction);
+    return {transaction: transaction, first_alert: first_alert};
+  } else {
+    if (first_alert) {
+      alert("Error: Could not apply " + key_coin + " price."); first_alert = false;
+    }
+    return {transaction: null, first_alert: first_alert};
+  }
+};
+
+function convertBinanceFeeTransaction(datetime, mainTransaction, fee, feeCoin, first_alert) {
+  var feeTransaction = {
+    "datetime" : formatBinanceDateTime(datetime, 2),  // set after 2sec from mainTransaction
+    "buyCoin" : mainTransaction["buyCoin"],
+    "buyAmount" : 0,
+    "sellCoin" : feeCoin,
+    "sellAmount" : fee,
+    "isAltTrade" : true,  // allways true for binance market tradeing
+    "altJPY" : "---",
+    "marketplace" : "Binance",
+    "comment" : "[Transaction Fee]"
+  };
+
+  var fee_coin_price = getJpyPrice(feeCoin, mainTransaction["datetime"].split(" ")[0]);
+  if (fee_coin_price != null) {
+    fee_rate = (fee * fee_coin_price) / (fee * fee_coin_price + mainTransaction["altJPY"]);
+    feeTransaction["buyAmount"] = mainTransaction["buyAmount"] * fee_rate;
+    mainTransaction["buyAmount"] = mainTransaction["buyAmount"] * (1 - fee_rate);
+    feeTransaction["altJPY"] = mainTransaction["altJPY"] * fee_rate;
+    mainTransaction["altJPY"] = mainTransaction["altJPY"] * (1 - fee_rate);
+    feeTransaction["comment"] += " " + feeCoin + "/JPY=" + fee_coin_price;
+    return {mainTransaction: mainTransaction, feeTransaction: feeTransaction, first_alert: first_alert};
+  } else {
+    if (first_alert) {
+      alert("Error: Could not apply " + feeCoin + " price."); first_alert = false;
+    }
+    return {mainTransaction: mainTransaction, feeTransaction: null, first_alert: first_alert};
+  }
+};
+
 function loadBinanceHistory(binance_history) {
   var l_history = [];
   var first_alert = true;
   for (var i=0; i<binance_history.length; ++i) {
-    var transaction = {
-      "datetime" : formatBinanceDateTime(binance_history[i].Date),
-      "buyCoin" :"",
-      "buyAmount" : 0,
-      "sellCoin" : "",
-      "sellAmount" : 0,
-      "isAltTrade" : true,  // allways true for binance market tradeing
-      "altJPY" : "---",
-      "marketplace" : "Binance",
-      "comment" : ("rate=" + binance_history[i].Price)
-    };
-    var market = "";
-    if (g_binance_bnb_trade_list.indexOf(binance_history[i].Market) != -1) {
-      market = "BNB";
-    } else if (g_binance_btc_trade_list.indexOf(binance_history[i].Market) != -1) {
-      market = "BTC";
-    } else if (g_binance_eth_trade_list.indexOf(binance_history[i].Market) != -1) {
-      market = "ETH";
-    } else if (g_binance_usdt_trade_list.indexOf(binance_history[i].Market) != -1) {
-      market = "USDT";
-    } else {
-      if (first_alert) {
-        alert("Error: Could not parse Market column."); first_alert = false;
+    var ret = convertBinanceTransaction(binance_history[i], first_alert);
+    first_alert = ret.first_alert;
+    if (ret.transaction != null) {
+      var mainTransaction = ret.transaction;
+      ret = convertBinanceFeeTransaction(binance_history[i].Date,
+                                         mainTransaction,
+                                         binance_history[i]["Fee"],
+                                         binance_history[i]["Fee Coin"],
+                                         first_alert);
+      first_alert = ret.first_alert;
+      var feeTransaction = ret.feeTransaction;
+      if (feeTransaction != null) {
+        l_history.push(mainTransaction);
+        l_history.push(feeTransaction);
       }
     }
-    var key_coin = market;
-    var target_coin = binance_history[i].Market.substring(0, binance_history[i].Market.indexOf(market));
-    if (getCoinAlias(target_coin) != null) {
-      target_coin = getCoinAlias(target_coin);
-    }
-    var key_coin_price = getJpyPrice(key_coin, transaction["datetime"].split(" ")[0]);
-    if (key_coin_price != null) {
-      if (binance_history[i].Type == "BUY") {
-        transaction["buyCoin"] = target_coin;
-        transaction["sellCoin"] = key_coin;
-        transaction["buyAmount"] = binance_history[i].Amount;
-        transaction["sellAmount"] = binance_history[i].Total;
-        transaction["altJPY"] = key_coin_price * binance_history[i].Total;
-      } else if (binance_history[i].Type == "SELL") {
-        transaction["buyCoin"] = key_coin;
-        transaction["sellCoin"] = target_coin;
-        transaction["buyAmount"] = binance_history[i].Total;
-        transaction["sellAmount"] = binance_history[i].Amount;
-        transaction["altJPY"] = key_coin_price * binance_history[i].Total;
-      } else {
-        if (first_alert) {
-          alert("Error: Could not parse Type column."); first_alert = false;
-        }
-      }
-      transaction["comment"] += ", " + key_coin + "/JPY=" + key_coin_price;
-      // console.log(binance_history[i]);
-      // console.log(transaction);
-      l_history.push(transaction);
-    } else {
-      if (first_alert) {
-        alert("Error: Could not apply " + key_coin + " price."); first_alert = false;
-      }
-    }
+
   }
   return l_history;
 }
